7bb94541bc59348d5f699781e118c3eb681030b8
Jeffrey J <yourzjf@126.com>
2022-12-20 16:09:21 +0800

    [FEATURE] 通过parent来管理spring-boot-starter的版本，新增userserv模块

---
+++ scdemo-userserv/src/main/java/net/yourzjf/scdemo/userserv/ScdemoUserServApplication.java
@@ -0,0 +1,13 @@
+package net.yourzjf.scdemo.userserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScdemoUserServApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScdemoUserServApplication.class, args);
+    }
+
+}
